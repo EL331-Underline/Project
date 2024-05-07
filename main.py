0a2444cdd9beff76c55162c6e06d6a1fc7d42081
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-05-07 17:50:13 +0900

    fix: fix bugs of level1-1

--- main.py
+++ main.py
@@ -2,31 +2,42 @@ import sqlite3
 import os
 
 
-# Connect to specified database and create `files` table if it not exist.
-# Returns `Connection` to `database`.
 def connect_database(database):
+    """
+    Connect to specified database and create `files` table if it not exist.
+    Returns `Connection` to `database`.
+    """
     con = sqlite3.connect(database)
     cur = con.cursor()
     res = cur.execute("""
         SELECT name
         FROM sqlite_master
-        WHERE type='table' AND name=files
+        WHERE type='table' AND name='files'
     """)
     if len(res.fetchall()) == 0:
-        cur.execute("CREATE TABLE files(org_name, cur_name, content)")
+        cur.execute("""
+            CREATE TABLE files(org_name TEXT, cur_name TEXT, content TEXT)
+        """)
         con.commit()
     cur.close()
     return con
 
 
 def create(path, database):
+    """
+    Read a content in `path` and store it to `database`.
+    `org_name` and `cur_name` will be the file name in `path`.
+    """
+    path = os.path.abspath(path)
     _, filename = os.path.split(path)
 
     con = connect_database(database)
     cur = con.cursor()
-    cur.execute("""
-        INSERT INTO files VALUES({0}, {0}, readfile({1}))
-    """.format(filename, path))
+    with open(path) as f:
+        # TODO: Hold the file path instead of file content for efficiency.
+        cur.execute("""
+            INSERT INTO files VALUES('{0}', '{0}', '{1}')
+        """.format(filename, f.read()))
     con.commit()
 
     cur.close()
@@ -34,32 +45,44 @@ def create(path, database):
 
 
 def retrieve(name, database):
+    """
+    Fetch the file content from `database` where the name is `name`.
+    """
     con = connect_database(database)
     cur = con.cursor()
     res = cur.execute("""
-        SELECT content FROM files WHERE cur_name={0}
+        SELECT content FROM files WHERE cur_name='{0}'
     """.format(name))
     content = res.fetchone()
+    assert content, "no such file exist on database, {0}".format(name)
     cur.close()
     con.close()
-    return content
+    return content[0]
 
 
 def update(old_name, new_name, database):
+    """
+    Update the file name so that user can fetch the content with `new_name`
+    """
     con = connect_database(database)
     cur = con.cursor()
     cur.execute("""
-        UPDATE files SET cur_name={1} WHERE cur_name={0}
+        UPDATE files SET cur_name='{1}' WHERE cur_name='{0}'
     """.format(old_name, new_name))
+    con.commit()
     cur.close()
     con.close()
 
 
 def delete(name, database):
+    """
+    Delete a file from database where the current name is `name`.
+    """
     con = connect_database(database)
     cur = con.cursor()
     cur.execute("""
-        DELETE FROM files WHERE cur_name={0}
+        DELETE FROM files WHERE cur_name='{0}'
     """.format(name))
+    con.commit()
     cur.close()
     con.close()
