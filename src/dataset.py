8dff79e1752207214da5d5ff59f1a624e021f8ff
pogyomo <pogyomo@gmail.com>
2024-05-19 20:33:51 +0900

    test(dataset): add tests

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import unittest
+from tempfile import NamedTemporaryFile
 
 
 def connect_database(database: str | os.PathLike) -> sqlite3.Connection:
@@ -30,8 +31,56 @@ def connect_database(database: str | os.PathLike) -> sqlite3.Connection:
 
 
 class TestConnectDatabase(unittest.TestCase):
-    def test_connect_database(self):
-        pass
+    def test_connect_database_with_empty_db(self):
+        with NamedTemporaryFile(suffix="db") as db:
+            con = connect_database(db.name)
+
+            cur = con.cursor()
+            res = cur.execute("""
+                SELECT name
+                FROM sqlite_master
+                WHERE type='table' AND name='files'
+            """)
+            names = res.fetchall()
+            cur.close()
+            assert len(names) == 1
+
+            # Check `connect_database` creates empty `files`
+            cur = con.cursor()
+            res = cur.execute("""
+                SELECT * FROM files
+            """)
+            files = res.fetchall()
+            cur.close()
+            assert len(files) == 0
+
+    def test_connect_database_with_non_empty_db(self):
+        with NamedTemporaryFile(suffix="db") as db:
+            # Create `files` table and insert a value.
+            con = sqlite3.connect(db.name)
+            cur = con.cursor()
+            cur.execute("""
+                CREATE TABLE files(org_name TEXT, cur_name TEXT, path TEXT)
+            """)
+            con.commit()
+            cur.execute("""
+                INSERT INTO files VALUES('test org', 'test cur', 'test path')
+            """)
+            con.commit()
+            cur.close()
+            con.close()
+
+            con = connect_database(db.name)
+
+            # Check if `files` table is not modified by `connect_database`.
+            cur = con.cursor()
+            res = cur.execute("""
+                SELECT * FROM files
+            """)
+            files = res.fetchall()
+            cur.close()
+            assert len(files) == 1
+            assert ("test org", "test cur", "test path") in files
 
 
 class DatasetManager:
@@ -116,19 +165,87 @@ class DatasetManager:
 
 class TestDatasetManager(unittest.TestCase):
     def test_create(self):
-        pass
+        with NamedTemporaryFile(suffix="db") as db, \
+                NamedTemporaryFile() as f1, \
+                NamedTemporaryFile() as f2:
+            m = DatasetManager(db.name)
+            m.create(f1.name, None)
+            m.create(f2.name, "f2-alias")
+
+            cur = m.con.cursor()
+            res = cur.execute("""
+                SELECT * FROM files
+            """)
+            files = res.fetchall()
+            cur.close()
+
+            f1_name = os.path.split(f1.name)[1]
+            f2_name = "f2-alias"
+            assert len(files) == 2
+            assert (f1_name, f1_name, f1.name) in files
+            assert (f2_name, f2_name, f2.name) in files
 
     def test_retrieve(self):
-        pass
+        with NamedTemporaryFile(suffix="db") as db, NamedTemporaryFile() as f:
+            m = DatasetManager(db.name)
+            m.create(f.name, "file")
+
+            with open(f.name, "w") as f:
+                f.write("test sentence")
+            fetched = m.retrieve("file")
+            assert fetched == "test sentence", fetched
 
     def test_retrieve_names(self):
-        pass
+        with NamedTemporaryFile(suffix="db") as db, \
+                NamedTemporaryFile() as f1, \
+                NamedTemporaryFile() as f2:
+            m = DatasetManager(db.name)
+            m.create(f1.name, "file1")
+            m.create(f2.name, "file2")
+            names = m.retrieve_names()
+            assert len(names) == 2
+            assert "file1" in names
+            assert "file2" in names
 
     def test_update(self):
-        pass
+        with NamedTemporaryFile(suffix="db") as db, NamedTemporaryFile() as f1:
+            m = DatasetManager(db.name)
+            m.create(f1.name, "file1")
+
+            m.update("file1", "new file1")
+
+            cur = m.con.cursor()
+            res = cur.execute("""
+                SELECT org_name, cur_name FROM files
+            """)
+            names = res.fetchall()
+            cur.close()
+            assert len(names) == 1
+            assert ("file1", "new file1") in names
 
     def test_delete(self):
-        pass
+        with NamedTemporaryFile(suffix="db") as db, NamedTemporaryFile() as f1:
+            m = DatasetManager(db.name)
+            m.create(f1.name, "file1")
+
+            cur = m.con.cursor()
+            res = cur.execute("""
+                SELECT * FROM files
+            """)
+            files = res.fetchall()
+            cur.close()
+            assert len(files) == 1
+            assert ("file1", "file1", f1.name) in files
+
+            m.delete("file1")
+
+            cur = m.con.cursor()
+            res = cur.execute("""
+                SELECT * FROM files
+            """)
+            files = res.fetchall()
+            cur.close()
+            assert len(files) == 0
 
 
 if __name__ == "__main__":
